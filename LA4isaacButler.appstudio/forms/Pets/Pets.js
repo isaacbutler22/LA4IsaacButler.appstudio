ff65a2526272bf02742d9d4ce0f7b766dd337c9b
isaacbutler22 <90641048+isaacbutler22@users.noreply.github.com>
2021-09-15 16:09:34 -0500

    updated LA4

---
+++ LA4isaacButler.appstudio/forms/Pets/Pets.js
@@ -0,0 +1,8 @@
+let petNames = ['Darcy', 'Jack', 'Jerry', 'Marty', 'Vito', 'Vinny', 'Riley', 'Saddie', 'Marge', 'Sam']
+let withSal = petNames
+withSal.push('sal')
+console.log(withSal)
+console.log('The third pets name is', withSal[2])
+let withSalLength = withSal.length
+console.log('The array has', withSalLength, 'names in it')
+withSal.pop()
